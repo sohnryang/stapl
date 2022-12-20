8f4bf11a09510a3936431aafcffad51283397534
Ryang Sohn <loop.infinitely@gmail.com>
2022-12-20 16:54:57 +0900

    Add read the docs support

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,8 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import subprocess
 
 project = "stapl"
 copyright = "2022, sohnryang"
@@ -31,3 +33,27 @@ html_theme_options = {
     "source_branch": "main",
 }
 html_static_path = ["_static"]
+
+
+def configure_doxyfile(input_dir, output_dir):
+    with open("Doxyfile.in") as f:
+        filedata = f.read()
+    replace_table = {
+        "@CMAKE_PROJECT_NAME@": "Stapl",
+        "@DOXYGEN_OUTPUT_DIR@": output_dir,
+        "@DOXYGEN_INPUT_DIR@": input_dir,
+    }
+    for pattern, value in replace_table:
+        filedata.replace(pattern, value)
+    with open("DOxyfile", "w") as f:
+        f.write(filedata)
+
+
+rtd_build = os.environ.get("READTHEDOCS", None) == "True"
+breathe_projects = {}
+if rtd_build:
+    input_dir = "../include"
+    output_dir = "build"
+    configure_doxyfile(input_dir, output_dir)
+    subprocess.call("doxygen", shell=True)
+    breathe_projects["Stapl"] = output_dir + "/xml"
