654e048bcab2de8e6bb3fc57453a0ac3c3c1a100
Ryang Sohn <loop.infinitely@gmail.com>
2023-02-11 14:25:50 +0900

    docs(parser): remove ad-hoc documentation

--- include/parser.h
+++ include/parser.h
@@ -12,7 +12,10 @@
 
 namespace stapl::parsing {
 /**
-  Parser for stapl.
+  Parser for stapl. This is a combination of [Recursive Descent
+  Parsing](https://en.wikipedia.org/wiki/Recursive_descent_parser) and
+  [Operator-Precedence
+  Parsing](https://en.wikipedia.org/wiki/Operator-precedence_parser).
  */
 class Parser {
 private:
@@ -27,118 +30,62 @@ public:
 
   /**
     Parse integer literals.
-
-    ```ebnf
-    intliteral = number ;
-    literal = intliteral | floatliteral ;
-    ```
    */
   ast::LiteralExprNode<int> parse_int();
 
   /**
     Parse floating-point literals.
-
-    ```ebnf
-    floatliteral = floatnumber ;
-    literal = intliteral | floatliteral ;
-    ```
    */
   ast::LiteralExprNode<double> parse_float();
 
   /**
     Parse expression and create an ``ExprNode``.
-
-    ```ebnf
-    expr = primary , binoprhs ;
-    ```
    */
   ast::ExprNode parse_expr();
 
   /**
     Parse an expression inside a paren.
-
-    ```ebnf
-    parenexpr = "(" , expr , ")" ;
-    ```
    */
   ast::ExprNode parse_paren_expr();
 
   /**
     Parse an identifier.
-
-    ```ebnf
-    identifier = identifier , "(" , [ expr ] , ")"
-               | identifier , "(" , expr , { "," , expr } , ")" ;
-    ```
    */
   ast::ExprNode parse_identifier();
 
   /**
     Parse a primary expression, which is one of identifier, literal and paren
     expression.
-
-    ```ebnf
-    primary = identifier | literal | parenexpr ;
-    ```
    */
   ast::ExprNode parse_primary();
 
   /**
     Parse a binary operator and rhs.
-
-    ```ebnf
-    operator = "+" | "-" | "*" | "/" | "==" | "!=" | ">" | ">=" | "<" | "<=" ;
-    binoprhs = { operator , primary }- ;
-    ```
    */
   ast::ExprNode parse_binop_rhs(int prec, ast::ExprNode lhs);
 
   /**
     Parse an ``if`` expression.
-
-    ```ebnf
-    ifexpr = "if" | expr | "then" | expr | "else" | expr ;
-    ```
    */
   ast::ExprNode parse_if();
 
   /**
     Parse a function prototype.
-
-    ```ebnf
-    typename = "int" | "float" ;
-    proto
-      = id , "(" , [ id , ":" , typename ] , ")" , ":" , typename
-      | id , "(" , id , ":" , typename , { "," , id , ":" , typename } , ")" ,
-        ":" , typename;
-    ```
    */
   ast::PrototypeNode parse_proto();
 
   /**
     Parse a ``def`` statement.
-
-    ```ebnf
-    funcdef = "def" , proto , expr ;
-    ```
    */
   ast::FunctionNode parse_def();
 
   /**
     Parse toplevel expression.
-
-    ```ebnf
-    toplevelexpr = expr ;
-    ```
    */
   ast::FunctionNode parse_toplevel_expr();
 
   /**
     Parse an extern function.
-
-    ```ebnf
-    externfunc = "extern" , proto ;
-    ```
    */
   ast::FunctionNode parse_extern();
 
