8ffa1f148b72b7f76a1b5952c1b67018595c783e
Ryang Sohn <loop.infinitely@gmail.com>
2023-07-09 00:45:51 +0900

    fix(ir): add missing include

--- src/irgen.cpp
+++ src/irgen.cpp
@@ -13,6 +13,7 @@
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Constant.h>
 #include <llvm/IR/Constants.h>
+#include <llvm/IR/Function.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/LLVMContext.h>
