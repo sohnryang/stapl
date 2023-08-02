e0331896d6c79b4aadc7bec13a963c97ee0003e6
Ryang Sohn <loop.infinitely@gmail.com>
2023-08-02 14:52:58 +0900

    docs(ast): imporve formatting of type names

--- include/ast_printer.h
+++ include/ast_printer.h
@@ -12,91 +12,91 @@ namespace stapl::ast {
 class ASTPrinter {
 public:
   /**
-   * @brief Represent LiteralExprNode<int> as string.
+   * @brief Represent ``LiteralExprNode<int>`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const LiteralExprNode<int> &node) const;
 
   /**
-   * @brief Represent LiteralExprNode<double> as string.
+   * @brief Represent ``LiteralExprNode<double>`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const LiteralExprNode<double> &node) const;
 
   /**
-   * @brief Represent LiteralExprNode<bool> as string.
+   * @brief Represent ``LiteralExprNode<bool>`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const LiteralExprNode<bool> &node) const;
 
   /**
-   * @brief Represent VariableExprNode as string.
+   * @brief Represent ``VariableExprNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const VariableExprNode &node) const;
 
   /**
-   * @brief Represent BinaryExprNode as string.
+   * @brief Represent ``BinaryExprNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const std::unique_ptr<BinaryExprNode> &node) const;
 
   /**
-   * @brief Represent CallExprNode as string.
+   * @brief Represent ``CallExprNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const std::unique_ptr<CallExprNode> &node) const;
 
   /**
-   * @brief Represent PrototypeNode as string.
+   * @brief Represent ``PrototypeNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const PrototypeNode &node) const;
 
   /**
-   * @brief Represent LetStmtNode as string.
+   * @brief Represent ``LetStmtNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const LetStmtNode &node) const;
 
   /**
-   * @brief Represent AssignmentStmtNode as string.
+   * @brief Represent ``AssignmentStmtNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const AssignmentStmtNode &node) const;
 
   /**
-   * @brief Represent IfStmtNode as string.
+   * @brief Represent ``IfStmtNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const std::unique_ptr<IfStmtNode> &node) const;
 
   /**
-   * @brief Represent ReturnStmtNode as string.
+   * @brief Represent ``ReturnStmtNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const ReturnStmtNode &node) const;
 
   /**
-   * @brief Represent CompoundStmtNode as string.
+   * @brief Represent ``CompoundStmtNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
   std::string operator()(const std::unique_ptr<CompoundStmtNode> &node) const;
 
   /**
-   * @brief Represent FunctionDeclNode as string.
+   * @brief Represent ``FunctionDeclNode`` as string.
    * @param node The node to represent.
    * @return The string representation of the node.
    */
